1f5d7dafca88fbb402869d6c891416c8df7567f2
ForteEscape <sehun5515@naver.com>
2023-07-13 20:36:25 +0900

    test: 도서 리뷰 및 회원 엔티티 상태 변경 메서드 테스트 구현 - 도서 리뷰 엔티티 리뷰 수정 메서드 기능 테스트 - 회원 엔티티 회원 이름, 주소 변경 메서드 기능 테스트 - 회원 엔티티 회원 도서 대여 가능 상태 변경 메서드 기능 테스트 - 회원 엔티티 패스워드 변경 메서드 기능 테스트

--- src/test/java/com/management/library/domain/BookReviewTest.java
+++ src/test/java/com/management/library/domain/BookReviewTest.java
@@ -1,10 +1,11 @@
 package com.management.library.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import com.management.library.domain.type.BookStatus;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,8 +20,9 @@ class BookReviewTest {
   private EntityManager em;
 
   @Test
-  @DisplayName("책 리뷰 테스트")
+  @DisplayName("책 리뷰 엔티티 저장 테스트")
   void bookReviewTest() {
+    // given
     Member memberA = Member.builder()
         .name("memberA")
         .build();
@@ -43,8 +45,54 @@ class BookReviewTest {
 
     em.persist(bookReview);
 
+    // when
     BookReview bookReview1 = em.find(BookReview.class, bookReview.getId());
 
-    Assertions.assertThat(bookReview1).isEqualTo(bookReview);
+    // then
+    assertThat(bookReview1).isEqualTo(bookReview);
+  }
+
+  @Test
+  @DisplayName("등록된 리뷰 수정 테스트")
+  void changeReviewAndContentTest(){
+    // given
+    Member memberA = Member.builder()
+        .name("memberA")
+        .build();
+
+    em.persist(memberA);
+
+    Book book = Book.builder()
+        .bookInfo(new BookInfo("bookA", "author", "publisher", "4f", "isbn", 2002))
+        .bookStatus(BookStatus.AVAILABLE)
+        .build();
+
+    em.persist(book);
+
+    BookReview bookReview = BookReview.builder()
+        .reviewTitle("review")
+        .content("content")
+        .book(book)
+        .member(memberA)
+        .build();
+
+    em.persist(bookReview);
+
+    em.flush();
+    em.clear();
+
+    // when
+    BookReview bookReview1 = em.find(BookReview.class, bookReview.getId());
+    bookReview1.changeReviewTitleAndContent("review Title2", "review content2");
+
+    em.flush();
+    em.clear();
+
+    BookReview result = em.find(BookReview.class, bookReview.getId());
+
+    // then
+    assertThat(result.getReviewTitle()).isEqualTo("review Title2");
+    assertThat(result.getContent()).isEqualTo("review content2");
+    assertThat(result.getRate()).isEqualTo(bookReview.getRate());
   }
 }
