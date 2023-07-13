b3ebb82a50794dc1ffd02ca2900a027308f3eab3
ForteEscape <sehun5515@naver.com>
2023-07-13 17:07:54 +0900

    feat: 도서 엔티티 상태 변경 메서드 구현 - 서비스 레이어에서 엔티티로 상태 변경을 위임하도록 구현하기 위해 메서드들 구현

--- src/main/java/com/management/library/domain/Book.java
+++ src/main/java/com/management/library/domain/Book.java
@@ -1,29 +1,28 @@
 package com.management.library.domain;
 
 import com.management.library.domain.type.BookStatus;
-import java.util.ArrayList;
-import java.util.List;
+import com.management.library.dto.BookUpdateDto;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "books")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SuperBuilder
-public class Book extends BaseEntity{
+@EntityListeners(AuditingEntityListener.class)
+public class Book extends BaseEntity {
 
   @Id
   @GeneratedValue
@@ -37,4 +36,30 @@ public class Book extends BaseEntity{
 
   @Enumerated(EnumType.STRING)
   private BookStatus bookStatus;
+
+  @Builder
+  public Book(Long id, BookInfo bookInfo, int typeCode, BookStatus bookStatus) {
+    this.id = id;
+    this.bookInfo = bookInfo;
+    this.typeCode = typeCode;
+    this.bookStatus = bookStatus;
+  }
+
+  // 도서 상태 변경 메서드
+  public void changeBookStatus(BookStatus bookStatus) {
+    this.bookStatus = bookStatus;
+  }
+
+  // 도서 정보 변경 메서드
+  public void changeBookData(BookUpdateDto.Request request) {
+    this.typeCode = request.getTypeCode();
+    this.bookInfo = BookInfo.builder()
+        .title(request.getTitle())
+        .author(request.getAuthor())
+        .publisher(request.getPublisher())
+        .location(request.getLocation())
+        .publishedYear(request.getPublishedYear())
+        .isbn(request.getIsbn())
+        .build();
+  }
 }
