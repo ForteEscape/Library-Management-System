171278c2bdd8707b7a1a110332ac3f68bfd35ef8
ForteEscape <sehun5515@naver.com>
2023-07-15 15:54:06 +0900

    feat: 엔티티 피드백 적용 - 도서관 요구 사항 요청 관련 - 도서관에 반입할 새로운 책 요청 엔티티 생성 - 도서관 운영 개선 방안 요청 엔티티 생성 - 각 요청 엔티티에 대한 결과 엔티티 생성 - 결과 엔티티에도 처분 결과를 표시할 RequestStatus 삽입

---
+++ src/main/java/com/management/library/domain/NewBookRequestResult.java
@@ -0,0 +1,56 @@
+package com.management.library.domain;
+
+import com.management.library.domain.type.RequestStatus;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class NewBookRequestResult extends BaseEntity {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "new_book_request_result_id")
+  private Long id;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "new_book_request_id")
+  private NewBookRequest newBookRequest;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "administrator_id")
+  private Administrator administrator;
+
+  private String resultPostTitle;
+  private String resultPostContent;
+
+  @Enumerated(EnumType.STRING)
+  private RequestStatus result;
+
+  @Builder
+  public NewBookRequestResult(Long id, NewBookRequest newBookRequest, Administrator administrator,
+      String resultPostTitle, String resultPostContent, RequestStatus result) {
+    this.id = id;
+    this.newBookRequest = newBookRequest;
+    this.administrator = administrator;
+    this.resultPostTitle = resultPostTitle;
+    this.resultPostContent = resultPostContent;
+    this.result = result;
+  }
+}
--- src/main/java/com/management/library/domain/type/RequestStatus.java
+++ src/main/java/com/management/library/domain/type/RequestStatus.java
@@ -1,5 +1,18 @@
 package com.management.library.domain.type;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.stream.Stream;
+
 public enum RequestStatus {
-  STAND_BY, ACCEPTED, REFUSED
+  STAND_BY, ACCEPTED, REFUSED;
+
+  // DTO 에서 값을 받을 때 대소문자 구분 없이 사용 가능하도록 역직렬화 후 삽입
+  @JsonCreator
+  public static RequestStatus parsing(String inputValue) {
+
+    return Stream.of(RequestStatus.values())
+        .filter(requestStatus -> requestStatus.toString().equals(inputValue.toUpperCase()))
+        .findFirst()
+        .orElse(null);
+  }
 }
