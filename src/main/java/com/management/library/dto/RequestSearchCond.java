e85da89bc2b54b676b98d32dc36bcf2bca3e0f1c
ForteEscape <sehun5515@naver.com>
2023-07-21 21:07:36 +0900

    feat: 검색 시 사용할 DTO 객체 구현

---
+++ src/main/java/com/management/library/dto/RequestSearchCond.java
@@ -0,0 +1,16 @@
+package com.management.library.dto;
+
+import com.management.library.domain.type.RequestStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestSearchCond {
+
+  private RequestStatus requestStatus;
+}
