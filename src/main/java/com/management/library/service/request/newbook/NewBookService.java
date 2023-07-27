38442d505956bba9c961367aafbb7d0c4d168fb0
ForteEscape <sehun5515@naver.com>
2023-07-27 17:19:44 +0900

    feat: 신간 요청 서비스 구현

--- src/main/java/com/management/library/service/request/newbook/NewBookService.java
+++ src/main/java/com/management/library/service/request/newbook/NewBookService.java
@@ -6,11 +6,14 @@ import static com.management.library.service.request.newbook.dto.NewBookRequestS
 
 import com.management.library.domain.member.Member;
 import com.management.library.domain.newbook.NewBookRequest;
+import com.management.library.dto.RequestSearchCond;
 import com.management.library.exception.NoSuchElementExistsException;
 import com.management.library.repository.member.MemberRepository;
 import com.management.library.repository.newbook.NewBookRequestRepository;
 import com.management.library.service.request.RedisRequestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,5 +36,13 @@ public class NewBookService {
     NewBookRequest savedRequest = newBookRequestRepository.save(NewBookRequest.of(request, member));
 
     return Response.of(savedRequest);
+  }
+
+  public Page<Response> getMemberNewBookRequest(String memberCode, Pageable pageable){
+    return newBookRequestRepository.findByMemberCode(memberCode, pageable);
+  }
+
+  public Page<Response> getAllNewBookRequest(RequestSearchCond cond, Pageable pageable){
+    return newBookRequestRepository.findAll(cond, pageable);
   }
 }
