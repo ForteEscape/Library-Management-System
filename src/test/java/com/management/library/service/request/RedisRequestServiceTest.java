025efb2527cce9cdf74f5b5b717373c1bf2692b9
ForteEscape <sehun5515@naver.com>
2023-07-28 18:07:47 +0900

    test: 하나의 요청에 대한 결과 게시글이 2개 이상 작성되지 않도록 하기 위한 redis 기능 구현 단위 테스트

--- src/test/java/com/management/library/service/request/RedisRequestServiceTest.java
+++ src/test/java/com/management/library/service/request/RedisRequestServiceTest.java
@@ -2,14 +2,17 @@ package com.management.library.service.request;
 
 import static com.management.library.exception.ErrorCode.MANAGEMENT_REQUEST_COUNT_EXCEEDED;
 import static com.management.library.exception.ErrorCode.NEW_BOOK_REQUEST_COUNT_EXCEEDED;
+import static com.management.library.exception.ErrorCode.REPLY_ALREADY_EXISTS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.entry;
 
 import com.management.library.AbstractContainerBaseTest;
+import com.management.library.exception.InvalidAccessException;
 import com.management.library.exception.RequestLimitExceededException;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,15 +28,20 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
   private RedisRequestService redisManagementRequestService;
   private static final String MANAGEMENT_CACHE_KEY = "management-request-count:";
   private static final String NEW_BOOK_CACHE_KEY = "book-request-count:";
+  private static final String NEW_BOOK_REQUEST_PREFIX = "book-request-id:";
+  private static final String MANAGEMENT_REQUEST_PREFIX = "management-request-id:";
+
   @AfterEach
-  void tearDown(){
+  void tearDown() {
     redisManagementRequestService.deleteCache(MANAGEMENT_CACHE_KEY);
     redisManagementRequestService.deleteCache(NEW_BOOK_CACHE_KEY);
+    redisManagementRequestService.deleteCache(NEW_BOOK_REQUEST_PREFIX + 1L);
+    redisManagementRequestService.deleteCache(MANAGEMENT_REQUEST_PREFIX + 1L);
   }
 
   @DisplayName("memberCode 로 등록한 운영 요청 횟수 캐시 값을 가져올 수 있다.")
   @Test
-  public void getManagementRequestRedisHashValue() throws Exception {
+  public void getManagementRequestRedisHashValue() {
     // given
     // when
     redisManagementRequestService.checkManagementRequestCount("1000001");
@@ -45,7 +53,7 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
 
   @DisplayName("저장된 여러 개의 memberCode 에 대해 memberCode 로 운영 요청 횟수 캐시 값을 가져올 수 있다.")
   @Test
-  public void getManagementRequestRedisHashValueWithMultiple() throws Exception {
+  public void getManagementRequestRedisHashValueWithMultiple() {
     // given
     // when
     redisManagementRequestService.checkManagementRequestCount("1000001");
@@ -65,10 +73,10 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
 
   @DisplayName("하나의 key에 대해 5번 카운트를 내린 상태에서 다시 카운트를 내리려고 하면 예외가 발생한다.")
   @Test
-  public void getManageRequestRedisHashValueWithExceedLimit() throws Exception {
+  public void getManageRequestRedisHashValueWithExceedLimit() {
     // given
     // when
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
       redisManagementRequestService.checkManagementRequestCount("1000001");
     }
 
@@ -83,7 +91,7 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
 
   @DisplayName("memberCode 로 등록한 신간 도서 요청 횟수 캐시 값을 가져올 수 있다.")
   @Test
-  public void getNewBookRedisHashValue() throws Exception {
+  public void getNewBookRedisHashValue() {
     // given
     // when
     redisManagementRequestService.checkNewBookRequestCount("1000001");
@@ -95,7 +103,7 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
 
   @DisplayName("저장된 여러 개의 memberCode 에 대해 memberCode 로 신간 도서 요청 횟수 캐시 값을 가져올 수 있다.")
   @Test
-  public void getNewBookRedisHashValueWithMultiple() throws Exception {
+  public void getNewBookRedisHashValueWithMultiple() {
     // given
     // when
     redisManagementRequestService.checkNewBookRequestCount("1000001");
@@ -115,10 +123,10 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
 
   @DisplayName("하나의 key에 대해 5번 카운트를 내린 상태에서 다시 카운트를 내리려고 하면 예외가 발생한다.")
   @Test
-  public void getNewBookRequestRedisHashValueWithExceedLimit() throws Exception {
+  public void getNewBookRequestRedisHashValueWithExceedLimit() {
     // given
     // when
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
       redisManagementRequestService.checkNewBookRequestCount("1000001");
     }
 
@@ -128,6 +136,123 @@ class RedisRequestServiceTest extends AbstractContainerBaseTest {
         .extracting("errorCode", "description")
         .contains(
             NEW_BOOK_REQUEST_COUNT_EXCEEDED, NEW_BOOK_REQUEST_COUNT_EXCEEDED.getDescription()
+        );
+  }
+
+  @DisplayName("신간 도서 요청 id로 캐시를 만들 수 있다.")
+  @Test
+  public void addBookRequestCache() {
+    // given
+    redisManagementRequestService.addBookRequestCache(1L);
+
+    // when
+    String result = redisTemplate.opsForValue().get(NEW_BOOK_REQUEST_PREFIX + 1L);
+
+    // then
+    assertThat(result).isEqualTo("");
+  }
+
+  @DisplayName("신간 도서 요청 id로 캐시를 제거할 수 있다.")
+  @Test
+  public void removeBookRequestCache() {
+    // given
+    redisManagementRequestService.addBookRequestCache(1L);
+
+    // when
+    // then
+    Assertions.assertDoesNotThrow(() -> redisManagementRequestService.removeBookRequestCache(1L));
+  }
+
+  @DisplayName("존재하지 않는 도서 id의 캐시를 제거하려고 하면 예외가 발생한다.")
+  @Test
+  public void removeBookRequestCacheWithIdNotExists() {
+    // given
+    redisManagementRequestService.addBookRequestCache(1L);
+
+    // when
+    // then
+    assertThatThrownBy(() -> redisManagementRequestService.removeBookRequestCache(5L))
+        .isInstanceOf(InvalidAccessException.class)
+        .extracting("errorCode", "description")
+        .contains(
+            REPLY_ALREADY_EXISTS, REPLY_ALREADY_EXISTS.getDescription()
+        );
+  }
+
+  @DisplayName("이미 캐시에서 제거한 도서 id의 캐시를 제거하려고 하면 예외가 발생한다.")
+  @Test
+  public void removeBookRequestCacheWithAlreadyDeletedId() {
+    // given
+    redisManagementRequestService.addBookRequestCache(1L);
+
+    // when
+    redisManagementRequestService.removeBookRequestCache(1L);
+
+    // then
+    assertThatThrownBy(() -> redisManagementRequestService.removeBookRequestCache(1L))
+        .isInstanceOf(InvalidAccessException.class)
+        .extracting("errorCode", "description")
+        .contains(
+            REPLY_ALREADY_EXISTS, REPLY_ALREADY_EXISTS.getDescription()
+        );
+  }
+
+  @DisplayName("운영 개선 요청 id로 캐시를 만들 수 있다.")
+  @Test
+  public void addManagementRequestCache() {
+    // given
+    redisManagementRequestService.addManagementRequestCache(1L);
+
+    // when
+    String result = redisTemplate.opsForValue().get(MANAGEMENT_REQUEST_PREFIX + 1L);
+
+    // then
+    assertThat(result).isEqualTo("");
+  }
+
+  @DisplayName("운영 개선 요청 id로 캐시를 제거할 수 있다.")
+  @Test
+  public void removeManagementRequestCache() {
+    // given
+    redisManagementRequestService.addManagementRequestCache(1L);
+
+    // when
+    // then
+    Assertions.assertDoesNotThrow(
+        () -> redisManagementRequestService.removeManagementRequestCache(1L));
+  }
+
+  @DisplayName("존재하지 않는 운영 개선 요청 id의 캐시를 제거하려고 하면 예외가 발생한다.")
+  @Test
+  public void removeManagementRequestCacheWithIdNotExists() {
+    // given
+    redisManagementRequestService.addManagementRequestCache(1L);
+
+    // when
+    // then
+    assertThatThrownBy(() -> redisManagementRequestService.removeManagementRequestCache(5L))
+        .isInstanceOf(InvalidAccessException.class)
+        .extracting("errorCode", "description")
+        .contains(
+            REPLY_ALREADY_EXISTS, REPLY_ALREADY_EXISTS.getDescription()
+        );
+  }
+
+  @DisplayName("이미 캐시에서 제거한 운영 개선 요청 id의 캐시를 제거하려고 하면 예외가 발생한다.")
+  @Test
+  public void removeManagementRequestCacheWithAlreadyDeletedId() {
+    // given
+    redisManagementRequestService.addManagementRequestCache(1L);
+
+    // when
+    redisManagementRequestService.removeManagementRequestCache(1L);
+
+    // then
+    assertThatThrownBy(() -> redisManagementRequestService.removeManagementRequestCache(1L))
+        .isInstanceOf(InvalidAccessException.class)
+        .extracting("errorCode", "description")
+        .contains(
+            REPLY_ALREADY_EXISTS, REPLY_ALREADY_EXISTS.getDescription()
         );
   }
 }
