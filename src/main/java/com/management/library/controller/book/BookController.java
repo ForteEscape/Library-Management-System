9c6f6c56ddac6d7722f85bcec28633f94f6253d4
ForteEscape <sehun5515@naver.com>
2023-07-30 16:01:42 +0900

    feat: 모든 책 조회 및 검색 기능 구현

--- src/main/java/com/management/library/controller/book/BookController.java
+++ src/main/java/com/management/library/controller/book/BookController.java
@@ -4,14 +4,19 @@ import static com.management.library.controller.book.dto.BookControllerCreateDto
 
 import com.management.library.controller.book.dto.BookControllerCreateDto;
 import com.management.library.controller.book.dto.BookControllerUpdateDto;
+import com.management.library.controller.book.dto.BookSearchCond;
 import com.management.library.service.book.BookService;
 import com.management.library.service.book.dto.BookServiceCreateDto;
 import com.management.library.service.book.dto.BookServiceUpdateDto;
 import com.management.library.service.book.dto.BookServiceResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +28,14 @@ public class BookController {
 
   private final BookService bookService;
 
-  @PostMapping("/new-book")
+  @PostMapping
   public Response addBook(@RequestBody BookControllerCreateDto.Request request) {
     BookServiceCreateDto.Response result = bookService.createNewBook(
         BookServiceCreateDto.Request.of(request));
     return Response.of(result);
   }
 
-  @PostMapping("/{bookId}/update")
+  @PutMapping("/{bookId}/update")
   public BookControllerUpdateDto.Response updateBook(@PathVariable("bookId") Long id,
       @RequestBody BookControllerUpdateDto.Request request) {
 
@@ -39,7 +44,12 @@ public class BookController {
     return BookControllerUpdateDto.Response.of(result);
   }
 
-  @PostMapping("/{bookId}/delete")
+  @GetMapping
+  public Page<BookServiceCreateDto.Response> getBookList(BookSearchCond cond, Pageable pageable){
+    return bookService.searchBook(cond, pageable);
+  }
+
+  @DeleteMapping("/{bookId}")
   public String deleteBook(@PathVariable("bookId") Long id) {
     return bookService.deleteBookData(id);
   }
