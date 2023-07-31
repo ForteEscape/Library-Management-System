323876c789959141f09659a4c9ad8f14da5134a6
ForteEscape <sehun5515@naver.com>
2023-07-31 22:29:17 +0900

    feat: 회원 통합 조회 서비스 구현(회원 정보 및 대여 가능 횟수, 이번 달 요청 등록 가능 횟수 등)

---
+++ src/main/java/com/management/library/service/query/MemberTotalInfoService.java
@@ -0,0 +1,36 @@
+package com.management.library.service.query;
+
+import com.management.library.service.member.MemberService;
+import com.management.library.service.member.dto.MemberReadServiceDto;
+import com.management.library.service.query.dto.MemberTotalInfoDto;
+import com.management.library.service.rental.RentalRedisService;
+import com.management.library.service.request.RedisRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberTotalInfoService {
+
+  private final RedisRequestService redisRequestService;
+  private final RentalRedisService rentalRedisService;
+  private final MemberService memberService;
+
+  public MemberTotalInfoDto getMemberTotalInfo(String memberCode) {
+    MemberReadServiceDto memberData = memberService.getMemberData(memberCode);
+    String managementRequestCount = redisRequestService.getManagementRequestCount(memberCode);
+    String newBookRequestCount = redisRequestService.getNewBookRequestCount(memberCode);
+    String memberRemainRentalCount = rentalRedisService.getMemberRemainRentalCount(memberCode);
+    String memberRentalStatus = "available";
+
+    if (rentalRedisService.checkMemberRentalPenalty(memberCode) ||
+        memberRemainRentalCount.equals("0")) {
+      memberRentalStatus = "unavailable";
+    }
+
+    return MemberTotalInfoDto.of(memberData, managementRequestCount, newBookRequestCount,
+        memberRemainRentalCount, memberRentalStatus);
+  }
+}
---
+++ src/main/java/com/management/library/service/query/dto/MemberTotalInfoDto.java
@@ -0,0 +1,56 @@
+package com.management.library.service.query.dto;
+
+import com.management.library.service.member.dto.MemberReadServiceDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemberTotalInfoDto {
+
+  private String name;
+  private String memberCode;
+  private String birthdayCode;
+  private String legion;
+  private String city;
+  private String street;
+  private String remainManagementRequestCount;
+  private String remainNewBookRequestCount;
+  private String remainRentalCount;
+  private String rentalStatus;
+
+  @Builder
+  private MemberTotalInfoDto(String name, String memberCode, String birthdayCode, String legion,
+      String city, String street, String remainManagementRequestCount,
+      String remainNewBookRequestCount, String remainRentalCount, String rentalStatus) {
+    this.name = name;
+    this.memberCode = memberCode;
+    this.birthdayCode = birthdayCode;
+    this.legion = legion;
+    this.city = city;
+    this.street = street;
+    this.remainManagementRequestCount = remainManagementRequestCount;
+    this.remainNewBookRequestCount = remainNewBookRequestCount;
+    this.remainRentalCount = remainRentalCount;
+    this.rentalStatus = rentalStatus;
+  }
+
+  public static MemberTotalInfoDto of(MemberReadServiceDto basicInfo, String managementCount,
+      String newBookCount, String remainRentalCount, String rentalStatus) {
+    return MemberTotalInfoDto.builder()
+        .name(basicInfo.getName())
+        .memberCode(basicInfo.getMemberCode())
+        .birthdayCode(basicInfo.getBirthdayCode())
+        .legion(basicInfo.getLegion())
+        .city(basicInfo.getCity())
+        .street(basicInfo.getStreet())
+        .remainManagementRequestCount(managementCount)
+        .remainNewBookRequestCount(newBookCount)
+        .remainRentalCount(remainRentalCount)
+        .rentalStatus(rentalStatus)
+        .build();
+  }
+}
