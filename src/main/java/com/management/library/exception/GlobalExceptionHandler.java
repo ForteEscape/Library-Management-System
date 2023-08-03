5e73d83ecfe164bb47aa5eda8c4f3125207b82ed
ForteEscape <sehun5515@naver.com>
2023-08-03 18:46:54 +0900

    refactor: response status 추가

--- src/main/java/com/management/library/exception/GlobalExceptionHandler.java
+++ src/main/java/com/management/library/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.management.library.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(DuplicateException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleDuplicateException(DuplicateException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -17,6 +20,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(InvalidAccessException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleInvalidAccessException(InvalidAccessException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -25,6 +29,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(InvalidArgumentException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleInvalidArgumentException(InvalidArgumentException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -33,6 +38,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(NoSuchElementExistsException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleNoSuchElementException(NoSuchElementExistsException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -41,6 +47,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(RedisLockException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleRedisLockException(RedisLockException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -49,6 +56,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(RentalException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleRentalException(RentalException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -57,6 +65,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(RequestLimitExceededException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleRequestLimitExceedException(RequestLimitExceededException e){
     return ErrorResponseDto.builder()
         .errorCode(e.getErrorCode())
@@ -65,6 +74,7 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ErrorResponseDto handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     BindingResult bindingResult = e.getBindingResult();
 
